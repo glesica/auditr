f4ed75755de7018485041bfdff1f9b16bd23c6cd
George Lesica <glesica@gmail.com>
2011-11-17 09:11:24 -0600

    Refactored main file to be more standard.

--- auditr/server.py
+++ auditr/server.py
@@ -3,18 +3,24 @@ import sys, logging
 import tornado.ioloop
 import tornado.web
 import tornado.database
+from tornado.options import define, options
 
 import handlers
 from config import *
-            
 
-if __name__ == '__main__':
-    debug = ('--debug' in sys.argv) or ('-d' in sys.argv)
 
+define('dbhost', default='localhost:3306', help='Database host', type=str)
+define('debug', default=False, help='Debugging mode', type=bool)
+define('port', default=8888, help='Port to run on', type=int)
+
+
+def main():
+    tornado.options.parse_command_line()
+    
     # Set up logging
     log_format = '%(asctime)s %(levelname)s (%(filename)s):%(message)s'
     
-    if debug:
+    if options.debug:
         logging.basicConfig(
             format=log_format, 
             level=logging.DEBUG
@@ -25,28 +31,27 @@ if __name__ == '__main__':
             format=log_format, 
             level=logging.WARNING
         )
-
-    logging.info('Connecting to database: %s' % DATABASE_HOST)
-
-    # Configure the database
+    
+    logging.info('Starting server on port %s' % SERVER_PORT)
+    
     database = tornado.database.Connection(
         DATABASE_HOST,
         DATABASE_NAME,
         DATABASE_USER,
         DATABASE_PASSWORD,
     )
-
-    # Configure URLs
     urls = [
         (r'/audits', handlers.AuditHandler, {'database': database}),
         (r'/computers', handlers.ComputerHandler, {'database': database}),
     ]
-
-    logging.info('Starting server on port %s' % SERVER_PORT)
-
-    application = tornado.web.Application(urls, debug=debug)
-    application.listen(SERVER_PORT)
+    
+    app = tornado.web.Application(urls, debug=options.debug)
+    app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
